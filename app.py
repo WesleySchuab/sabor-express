9700d94cf4b014c3af2b5ed29be7e2caf2bb7f49
Wesley <yinnihisoka@gmail.com>
2024-02-01 16:35:06 -0300

    var com padrao snake case

--- app.py
+++ app.py
@@ -3,4 +3,7 @@ print('SAbor express\n')
 print('1. Cadastrar restaurante')
 print('2. Listar restaurante')
 print('3. Ativarrestaurante')
-print('4. Sair')
+print('4. Sair\n')
+
+opcao_escolhida =input('escolha uma opção: ')
+print('4. Voce escolheu a opção \n',opcao_escolhida)
