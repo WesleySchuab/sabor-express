3177330ce4233323ff65a80537fded6c34b07c47
Wesley <yinnihisoka@gmail.com>
2024-02-01 17:20:37 -0300

    Definindo entrada como int

--- app.py
+++ app.py
@@ -12,6 +12,15 @@ print('2. Listar restaurante')
 print('3. Ativar restaurante')
 print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
+opcao_escolhida = int( input('Escolha uma opção: '))
 print(f'Você escolheu a opção {opcao_escolhida}')
 
+
+if opcao_escolhida == 1:
+    print('Cadastrar restaurante')
+elif opcao_escolhida == 2:
+    print('Listar restaurantes')
+elif opcao_escolhida == 3:
+    print('Ativar restaurantes')
+else:
+    print('Encerrando o programa')
