c824d224467f8e526934beabed3a3c81b6a8b344
Wesley <yinnihisoka@gmail.com>
2024-02-12 11:42:14 -0300

    criando metodo __SRT__

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -3,6 +3,15 @@ class Restaurante:
        self.nome = nome
        self.categoria = categoria
        self.ativo = False
+       
+    """
+    O método __str__ é um método especial que pega o objeto e define que, se precisarmos mostrar esse objeto em formato de texto,
+    mostraremos determinada informação. Podemos escolher se queremos mostrar o nome, a categoria ou o ativo. Ou seja, podemos escolher
+    e definir. 
+    Então, em vez de mostrar a representação do endereço de memória, ele mostrará o que decidirmos.
+    """
+    def __str__(self) -> str:
+        return self.nome
     nome = ''
     categoria = ''
     ativo = False
