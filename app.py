ac869d00769b25cc7870e19e8504346919c0e817
Wesley <yinnihisoka@gmail.com>
2024-02-14 16:32:21 -0300

    alterando atributos dos restaurantes

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from modelos.restaurante import Restaurante
 restaurante_praca = Restaurante('praça','Goumert')
 restaurante_mexicano = Restaurante('mexican','food')
 restaurante_japones = Restaurante('japa','Japoensa')
+
+restaurante_mexicano.alternar_estado()
 def main():
     Restaurante.listar_restaurantes()
 if __name__ == '__main__':
